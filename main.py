d9dcc78527287b35339d3bac653407e4645dfb59
Woon <hweewoon96@gmail.com>
2022-08-24 22:47:31 +0800

    initial commit

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SEC_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
